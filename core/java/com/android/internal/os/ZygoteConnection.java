0b5ffaf447457e51ce1580dc2a743209f94f0bb3
MSe <mse1969@posteo.de>
2023-07-28 14:53:44 +0200

    Zygote: Fix excec spawning system property to match with settings
    Change-Id: Iafb902f9c3b70545ec86f2b0a4ad44da3128e8c6

--- core/java/com/android/internal/os/ZygoteConnection.java
+++ core/java/com/android/internal/os/ZygoteConnection.java
@@ -248,8 +248,8 @@ class ZygoteConnection {
                     fdsToClose[1] = zygoteFd.getInt$();
                 }
 
-                if (parsedArgs.mInvokeWith != null || SystemProperties.getBoolean("sys.spawn.exec", false) || parsedArgs.mStartChildZygote
-                        || !multipleOK || peer.getUid() != Process.SYSTEM_UID) {
+                if (parsedArgs.mInvokeWith != null || SystemProperties.getBoolean("persist.security.exec_spawn_new", false)
+                        || parsedArgs.mStartChildZygote || !multipleOK || peer.getUid() != Process.SYSTEM_UID) {
                     // Continue using old code for now. TODO: Handle these cases in the other path.
                     pid = Zygote.forkAndSpecialize(parsedArgs.mUid, parsedArgs.mGid,
                             parsedArgs.mGids, parsedArgs.mRuntimeFlags, rlimits,
@@ -536,7 +536,7 @@ class ZygoteConnection {
             throw new IllegalStateException("WrapperInit.execApplication unexpectedly returned");
         } else {
             if (!isZygote) {
-                if (SystemProperties.getBoolean("sys.spawn.exec", false) &&
+                if (SystemProperties.getBoolean("persist.security.exec_spawn_new", false) &&
                         (parsedArgs.mRuntimeFlags & ApplicationInfo.FLAG_DEBUGGABLE) == 0) {
                     ExecInit.execApplication(parsedArgs.mNiceName, parsedArgs.mTargetSdkVersion,
                             VMRuntime.getCurrentInstructionSet(), parsedArgs.mRuntimeFlags, parsedArgs.mRemainingArgs);
